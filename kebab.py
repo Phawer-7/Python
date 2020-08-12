7bc33b62ed51ab551f78ebc57feb22464f453c9e
Phawer <69140238+Phawer-7@users.noreply.github.com>
2020-08-12 15:27:18 +0000

    0.1

--- kebab.py
+++ kebab.py
@@ -6,9 +6,10 @@ def kebab(s):
     sub(r"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+",
     lambda mo: ' ' + mo.group(0).lower(), s)).split())
 
+what_u_want = input('?:')
+print(kebab(what_u_want))
 
+#print(kebab('some-mixed_string With spaces_underscores-and-hyphens'))
+# 'some-mixed-string-with-spaces-underscores-and-hyphens'
+# kebab('AllThe-small Things') # "all-the-small-things"
 
-kebab('camelCase') # 'camel-case'
-kebab('some text') # 'some-text'
-kebab('some-mixed_string With spaces_underscores-and-hyphens') # 'some-mixed-string-with-spaces-underscores-and-hyphens'
-kebab('AllThe-small Things') # "all-the-small-things"
